76724a972d71fd8f35c797233709c5dcfe8488f5
Grégory Thognard <drangoht@gmail.com>
2024-06-28 01:27:57 +0200

    comment

--- DependencyContainer/DependencyContainer/Program.cs
+++ DependencyContainer/DependencyContainer/Program.cs
@@ -10,13 +10,13 @@ services.AddSingleton<IDummyPrint, DummyPrintTata>();
 services.AddTransient<DummyClassWithCtorInjection>();
 var container = services.GetContainer();
 
-var svc1 = container.GetService<DummyClass>();
-var svc2 = container.GetService<DummyClass>();
-Console.WriteLine(svc1.RandomGuid);
-Console.WriteLine(svc2.RandomGuid);
+//var svc1 = container.GetService<DummyClass>();
+//var svc2 = container.GetService<DummyClass>();
+//Console.WriteLine(svc1.RandomGuid);
+//Console.WriteLine(svc2.RandomGuid);
 
-var svc3 = container.GetService<IDummyPrint>();
-svc3.Print();
+//var svc3 = container.GetService<IDummyPrint>();
+//svc3.Print();
 
 var final = container.GetService<DummyClassWithCtorInjection>();
 final.PrintIt();
